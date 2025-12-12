9c9c28eae30dd341e488658cb8697e603d30a7be
Tucker Noia <tucker@yelp.com>
2025-12-12 14:02:16 -0800

    tabs are gross

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,17 +11,17 @@ vim.g.python3_host_prog = vim.g.neovim_python3_venv .. '/bin/python3'
 vim.g.black_virtualenv = vim.g.neovim_python3_venv
 
 if vim.fn.isdirectory(vim.g.neovim_python3_venv) == 0 then
-	print('Creating neovim Python virtualenv...')
-	vim.fn.execute('!~/.venv-setup.sh')
+    print('Creating neovim Python virtualenv...')
+    vim.fn.execute('!~/.venv-setup.sh')
 end
 
 local data_dir = vim.fn.stdpath('data') .. '/site'
 if vim.fn.empty(vim.fn.glob(data_dir .. '/autoload/plug.vim')) > 0 then
-	vim.fn.system({
-		'curl', '-fLo', data_dir .. '/autoload/plug.vim', '--create-dirs',
-		'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-	})
-	vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
+    vim.fn.system({
+        'curl', '-fLo', data_dir .. '/autoload/plug.vim', '--create-dirs',
+        'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    })
+    vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
 end
 
 -- Basic settings
@@ -35,7 +35,7 @@ vim.opt.undofile = true
 local undodir = vim.fn.expand('~/.vim/undo')
 vim.opt.undodir = undodir
 if vim.fn.isdirectory(undodir) == 0 then
-	vim.fn.mkdir(undodir, 'p')
+    vim.fn.mkdir(undodir, 'p')
 end
 
 -- Tabs and indentation
@@ -77,29 +77,29 @@ vim.opt.writebackup = false
 -- List chars
 vim.opt.list = true
 vim.opt.listchars = {
-	tab = '→~',
-	nbsp = '␣',
-	trail = '•',
-	precedes = '«',
-	extends = '»'
+    tab = '→~',
+    nbsp = '␣',
+    trail = '•',
+    precedes = '«',
+    extends = '»'
 }
 
 vim.opt.termguicolors = true
 
 -- Filetype settings
 vim.filetype.add({
-	extension = {
-		md = 'markdown',
-	}
+    extension = {
+        md = 'markdown',
+    }
 })
 
 -- Filetype-specific indentation
 vim.api.nvim_create_autocmd('FileType', {
-	pattern = {'yaml', 'lua'},
-	callback = function()
-		vim.opt_local.shiftwidth = 2
-		vim.opt_local.tabstop = 2
-	end
+    pattern = {'yaml', 'lua'},
+    callback = function()
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.tabstop = 2
+    end
 })
 
 -- Abbreviations
@@ -175,11 +175,11 @@ vim.g.airline_powerline_fonts = 1
 
 -- Copilot settings (only if available)
 if vim.fn.exists('*copilot#Accept') == 1 then
-	vim.keymap.set('i', '<C-y>', 'copilot#Accept("\\<CR>")', { expr = true, silent = true, script = true })
-	vim.g.copilot_no_tab_map = true
-	vim.keymap.set('i', '<C-]>', '<Plug>(copilot-next)')
-	vim.keymap.set('i', '<C-[>', '<Plug>(copilot-previous)')
-	vim.keymap.set('i', '<C-\\>', '<Plug>(copilot-dismiss)')
+    vim.keymap.set('i', '<C-y>', 'copilot#Accept("\\<CR>")', { expr = true, silent = true, script = true })
+    vim.g.copilot_no_tab_map = true
+    vim.keymap.set('i', '<C-]>', '<Plug>(copilot-next)')
+    vim.keymap.set('i', '<C-[>', '<Plug>(copilot-previous)')
+    vim.keymap.set('i', '<C-\\>', '<Plug>(copilot-dismiss)')
 end
 
 -- Keybindings
@@ -209,100 +209,100 @@ keymap('i', 'jk', '<Esc>', opts)
 require('which-key').setup()
 
 require('nvim-tree').setup({
-	renderer = {
-		group_empty = true,
-	},
+    renderer = {
+        group_empty = true,
+    },
 })
 
 require('aerial').setup({
-	backends = { 'lsp', 'treesitter' },
-	layout = {
-		default_direction = 'prefer_right',
-	},
-	on_attach = function(bufnr)
-		vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
-		vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
-	end,
+    backends = { 'lsp', 'treesitter' },
+    layout = {
+        default_direction = 'prefer_right',
+    },
+    on_attach = function(bufnr)
+        vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+        vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
+    end,
 })
 
 local has_treesitter, treesitter = pcall(require, 'nvim-treesitter.configs')
 if has_treesitter then
-	treesitter.setup({
-		ensure_installed = { 'python', 'lua', 'vim', 'vimdoc' },
-		highlight = { enable = true },
-		indent = { enable = true },
-	})
+    treesitter.setup({
+        ensure_installed = { 'python', 'lua', 'vim', 'vimdoc' },
+        highlight = { enable = true },
+        indent = { enable = true },
+    })
 end
 
 require('mason').setup()
 require('mason-lspconfig').setup({
-	ensure_installed = { 'pyright', 'lua_ls' },
-	automatic_installation = true,
+    ensure_installed = { 'pyright', 'lua_ls' },
+    automatic_installation = true,
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 vim.lsp.config.pyright = {
-	cmd = { 'pyright-langserver', '--stdio' },
-	filetypes = { 'python' },
-	root_markers = { 'pyproject.toml', 'setup.py', 'requirements.txt', '.git' },
-	capabilities = capabilities,
+    cmd = { 'pyright-langserver', '--stdio' },
+    filetypes = { 'python' },
+    root_markers = { 'pyproject.toml', 'setup.py', 'requirements.txt', '.git' },
+    capabilities = capabilities,
 }
 
 vim.lsp.config.lua_ls = {
-	cmd = { 'lua-language-server' },
-	filetypes = { 'lua' },
-	root_markers = { '.luarc.json', '.luarc.jsonc', '.luacheckrc', '.git' },
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { 'vim' },
-			},
-		},
-	},
-	capabilities = capabilities,
+    cmd = { 'lua-language-server' },
+    filetypes = { 'lua' },
+    root_markers = { '.luarc.json', '.luarc.jsonc', '.luacheckrc', '.git' },
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' },
+            },
+        },
+    },
+    capabilities = capabilities,
 }
 
 vim.lsp.enable({ 'pyright', 'lua_ls' })
 
 local null_ls = require('null-ls')
 null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.black.with({
-			extra_args = { '--line-length', '131' },
-		}),
-		null_ls.builtins.formatting.isort,
-	},
+    sources = {
+        null_ls.builtins.formatting.black.with({
+            extra_args = { '--line-length', '131' },
+        }),
+        null_ls.builtins.formatting.isort,
+    },
 })
 
 -- Diagnostic configuration
 vim.diagnostic.config({
-	float = {
-		source = 'always',
-		border = 'rounded',
-	},
+    float = {
+        source = 'always',
+        border = 'rounded',
+    },
 })
 
 -- Show diagnostics on hover
 vim.api.nvim_create_autocmd('CursorHold', {
-	callback = function()
-		vim.diagnostic.open_float(nil, { focusable = false })
-	end,
+    callback = function()
+        vim.diagnostic.open_float(nil, { focusable = false })
+    end,
 })
 
 -- LSP keybindings
 vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(args)
-		local bufmap = function(mode, lhs, rhs)
-			vim.keymap.set(mode, lhs, rhs, { buffer = args.buf })
-		end
-		bufmap('n', 'gd', vim.lsp.buf.definition)
-		bufmap('n', 'gr', vim.lsp.buf.references)
-		bufmap('n', 'K', vim.lsp.buf.hover)
-		bufmap('n', '<leader>rn', vim.lsp.buf.rename)
-		bufmap('n', '<leader>ca', vim.lsp.buf.code_action)
-		bufmap('n', '<leader>F', vim.lsp.buf.format)
-	end,
+    callback = function(args)
+        local bufmap = function(mode, lhs, rhs)
+            vim.keymap.set(mode, lhs, rhs, { buffer = args.buf })
+        end
+        bufmap('n', 'gd', vim.lsp.buf.definition)
+        bufmap('n', 'gr', vim.lsp.buf.references)
+        bufmap('n', 'K', vim.lsp.buf.hover)
+        bufmap('n', '<leader>rn', vim.lsp.buf.rename)
+        bufmap('n', '<leader>ca', vim.lsp.buf.code_action)
+        bufmap('n', '<leader>F', vim.lsp.buf.format)
+    end,
 })
 
 -- nvim-cmp setup
@@ -310,52 +310,53 @@ local cmp = require('cmp')
 local luasnip = require('luasnip')
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }),
-		['<Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { 'i', 's' }),
-		['<S-Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { 'i', 's' }),
-	}),
-	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		{ name = 'luasnip' },
-		{ name = 'buffer' },
-		{ name = 'path' },
-	})
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Esc>'] = cmp.mapping.abort(),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'path' },
+    })
 })
 
 -- Command-line completion
 cmp.setup.cmdline(':', {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = 'path' }
-	}, {
-		{ name = 'cmdline' }
-	})
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    })
 })
 
 -- Colorscheme
